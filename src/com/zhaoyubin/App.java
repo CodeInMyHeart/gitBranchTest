c4a70a03dcb0d86a399b261b2c61c1208cf0108a
赵玉彬 <86zhaoyubin@gmail.com>
2020-11-14 13:40:02 +0800

    bug branch

--- src/com/zhaoyubin/App.java
+++ src/com/zhaoyubin/App.java
@@ -1,4 +1,9 @@
 package com.zhaoyubin;
 
+import com.sun.tools.javac.Main;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("bug分支给master分支添加");
+    }
 }
