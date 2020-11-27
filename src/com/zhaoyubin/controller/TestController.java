b40515bedb813aa3fff0faa0ac246004668b7a2d
CodeInMyHeart <38681104+CodeInMyHeart@users.noreply.github.com>
2020-11-27 11:23:22 +0800

    Update TestController.java

--- src/com/zhaoyubin/controller/TestController.java
+++ src/com/zhaoyubin/controller/TestController.java
@@ -4,6 +4,10 @@ public class TestController {
     public void test(){
         int i =10;
         if(i==10){
+            if(i==10){
+                System.out.println("master冲突解决");
+            }
+      
             System.out.println("master冲突解决");
         }
     }
