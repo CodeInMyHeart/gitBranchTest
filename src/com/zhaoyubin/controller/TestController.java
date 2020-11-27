a28a8252c40109f9a11a20d325efb298fb9ad807
赵玉彬 <86zhaoyubin@gmail.com>
2020-11-27 11:19:04 +0800

    冲突测试

--- src/com/zhaoyubin/controller/TestController.java
+++ src/com/zhaoyubin/controller/TestController.java
@@ -2,6 +2,9 @@ package com.zhaoyubin.controller;
 
 public class TestController {
     public void test(){
-
+        int i =10;
+        if(i==10){
+            System.out.println("master冲突解决");
+        }
     }
 }
